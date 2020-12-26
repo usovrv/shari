fc1ab42d943635980f2921056c8ed795727b098a
Usov Roman <romus.us@yandex.ru>
2020-12-26 15:22:37 +0300

    Правки корзины

---
+++ .history/404_20201221224013.php
@@ -0,0 +1,25 @@
+<?
+if ($_SERVER['DOCUMENT_URI'] == "/404.php") {
+    $_SERVER['REQUEST_URI'] = $_SERVER['DOCUMENT_URI'];
+}
+include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
+
+CHTTP::SetStatus("404 Not Found");
+@define("ERROR_404","Y");
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+$APPLICATION->SetTitle("Страница не найдена!");
+?>
+
+<div class="b-default">
+	<div class="wrapper">
+		<div class="b-default__main">
+			<h1><?$APPLICATION->ShowTitle(false);?></h1>
+		</div>
+		<div class="clear"></div>
+	</div>	
+</div>
+<?
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
